842a46f66e637bb653c1f8368bea4985fdf799e5
ENICharles <charles.ravinet2021@campus-eni.fr>
2022-05-03 11:12:12 +0200

    Initial commit

---
+++ app/src/main/java/fr/eni/crt/cadox/ArticleViewActivity.java
@@ -0,0 +1,15 @@
+package fr.eni.crt.cadox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ArticleViewActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_article_view);
+    }
+}
